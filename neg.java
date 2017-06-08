3193d26e30bc751fb930c69508e07c43397d0dc9
AravindhanE <aravindhkarthi97@gmail.com>
2017-06-08 11:47:25 +0530

    Create neg.java

---
+++ neg.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class neg {
+	public static void main (String[] args) {
+	 Scanner s=new Scanner(System.in);
+	 int n=s.nextInt();
+	  
+	 if(n>0)
+            { 
+             System.out.println("positive");
+             }
+           else if(n<0)
+              { 
+                  System.out.println("negative");
+               }
+               else
+               {
+                    System.out.println("zero");
+               }
+	}
+}
